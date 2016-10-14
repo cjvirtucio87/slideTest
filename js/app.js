921421fe86dddd40721705517f96aba308cb918e
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-13 19:39:03 -0700

    updated slides to find parents

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// remove cdn for angular in html
+var app = angular.module('anglifyEdit', []);
+
+app.factory('$', ['$window', function($window){
+  return $window.$;
+}]);
+
+app.controller('pageWatchCtrl', ['$scope', '$', function($scope, $){
+  var elements = $('body *');
+  for (var i =1; i < ( 1 + elements.length ); i++){
+    $(elements[i]).attr('data-id', i);
+  }
+
+  $scope.states = [0];
+
+  $scope.currentState = 0;
+  $scope.createSlide = function($event){
+    var slideTag = $($event.currentTarget).first().parent();
+    $scope.states.push(slideTag.attr('data-id'));
+  };
+
+}]);
--- js/directives/show_hide/show_hide.js
+++ js/directives/show_hide/show_hide.js
@@ -3,12 +3,14 @@ app.directive('showHide', function () {
   var showHide = {};
   var _scope = {};
 
-  function _showHideLink (scope) {
+  function _showHideLink (scope, el, attr) {
+    console.log(attr);
     scope.flag = true;
     scope.toggle = function () {
       scope.flag = !scope.flag;
     };
   }
+
 
   showHide.restrict = 'A';
   showHide.scope = _scope;
