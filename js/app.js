3271f7095e55cbb16fe01ffafe314f1504369f3b
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-14 11:57:28 -0700

    removed extra comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // remove cdn for angular in html
 var app = angular.module('anglifyEdit', ['ngAnimate']);
 
+// note to remove jQuery from application
 app.factory('$', ['$window', function($window){
   return $window.$;
 }]);
